e32f9148754b353fbdc8aa0e3f613db37c803c75
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-03 12:43:07 +0800

    Add new card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+import MapPin from "../assets/map-pin.svg";
+import MapIcon from "../assets/map.svg";
+export default function Card(props) {
+    return (
+        <div className='card'>
+        </div>
+    );
+}
