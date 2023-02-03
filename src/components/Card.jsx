8429cda0253b168612c2d1c6fc93625031c4779d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-03 13:40:19 +0800

    Refactor: enclose elements inside div

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,14 +7,16 @@ export default function Card(props) {
             <div className='card--image'>
                 <img src={props.imageUrl} alt='{props.title}' />
             </div>
-            <div>
-                <div className='card--location'>
-                    <img src={MapPin} alt='' />
-                    <h3>{props.location}</h3>
-                </div>
-                <div className='card--google-maps'>
-                    <img src={MapIcon} alt='' />
-                    <a href='props.googleMapsUrl'>View on Google Maps</a>
+            <div className='card--info'>
+                <div>
+                    <div className='card--location'>
+                        <img src={MapPin} alt='' />
+                        <h3>{props.location}</h3>
+                    </div>
+                    <div className='card--google-maps'>
+                        <img src={MapIcon} alt='' />
+                        <a href='props.googleMapsUrl'>View on Google Maps</a>
+                    </div>
                 </div>
                 <h1 className='card--title'>{props.title}</h1>
                 <p className='card--dates'>
