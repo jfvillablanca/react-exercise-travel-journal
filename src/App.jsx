5746aa71418f41c655a3b3005131e9f3e6f1e2d2
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-02-03 12:43:53 +0800

    Import Card comp to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
+import Card from "./components/Card.jsx";
+import data from "./assets/data.js";
 
 function App() {
+    const cards = data.map((card) => {
+        return <Card key={card.id} {...card} />;
+    });
     return (
         <>
             <Navbar />
+            <main>{cards}</main>
         </>
     );
 }
